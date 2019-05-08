65bc92da0ff003f538069778a631294c7854c552
belchanin <belchanin@list.ru>
2019-05-08 23:56:16 +0300

    Исправляет ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -91,8 +91,7 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
-    "source/*.ico"
+    "source/js/**"
   ], {
     base: "source"
   })
